3ef0679967f90c2b3817f0d5e5f529c3903fabe4
DustieCBU <caleb.aragones@calbaptist.edu>
2022-05-03 18:11:04 -0700

    Testing toNum() and toChar() with Maps

--- src/toNumTest.java
+++ src/toNumTest.java
@@ -1,12 +1,44 @@
+import java.util.TreeMap;
+
 public class toNumTest {
-    private static final int SEED = 0;
+    private static final int SEED = 2;
+
     public static void main(String[] args) {
         System.out.println("To Num Test");
-        char letter = 'b';
-        int num;
+        System.out.println("SEED >>> " + SEED);
+        
+        testNums();
+        testChars();      
+        
+    }
 
-        toNum('a');
-//        toNum('b');
+    public static void testNums() {
+        String letters = "abcdefghijklmnopqurstuvwxyz";
+        char[] letterArr = letters.toCharArray();
+        int count = SEED;
+        TreeMap<Character, Integer> nums = new TreeMap<>();
+
+        for (char c : letterArr) {
+            nums.put(c, count);
+            count++;
+        }
+        System.out.println("Testing toNums()...");
+        System.out.println(nums.toString());
+    }
+
+    public static void testChars() {
+        int[] intArr = new int[26];
+        int count = SEED;
+        TreeMap<String, Integer> chars = new TreeMap<>();
+
+        for (int i = 0; i < 26 ; i++) {
+            intArr[i] = i;
+            chars.put(toChar(intArr[i]), SEED + i);
+        }
+
+        System.out.println("Testing toChar()...");
+        System.out.println(chars.toString());
+
 
     }
 
@@ -15,18 +47,15 @@ public class toNumTest {
 
         int quot = num / 26;
         int rem = num % 26;
-        char letter = (char)((int)'a' + rem + SEED); // TODO: <<<scuffed algorithm>>>
+        char letter = (char)((int)'a' + rem); // TODO: <<<scuffed algorithm>>>
 
         if (quot == 0) return "" + letter;
         else return toChar(quot - 1) + letter;
     }
 
-    public static void toNum(char in) {
-        int intValue = in; // returns ascii
-        char char1 = 'b';
-        int num = char1 - 0;
-        System.out.println("Inputted Character: " + in + "\nInteger value: " + intValue);
-
-        System.out.println(num - '0' - 49 - SEED);
+    public static int toNum(char in) {
+        return in  - 'a';
     }
+
+
 }
